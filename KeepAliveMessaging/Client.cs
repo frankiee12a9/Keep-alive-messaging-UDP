c7936ccdb5273b7ce007cb82f852224b2b36f1ff
TKNguyen12a9 <cudayanh@gmail.com>
2023-04-19 17:39:58 +0900

    chore

--- KeepAliveMessaging/Client.cs
+++ KeepAliveMessaging/Client.cs
@@ -16,7 +16,7 @@ namespace KeepAliveMessaging
 			UdpClient udpClient = new UdpClient();
 
 			// Endpoint for the server's IP address and port
-			IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(Util.loopback), 2345);
+			IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2345);
 
 			// A threadPool to send Keep Alive messages to the server every 5 seconds
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
@@ -24,7 +24,7 @@ namespace KeepAliveMessaging
 			{
 				while (true)
 				{
-					Console.WriteLine("Sending Keep Alive request to server");
+					Console.WriteLine("Sending Keep-Alive request to server");
 
 					// Send a Keep Alive request to the server
 					byte[] data = new byte[] { 0xFF };
@@ -54,7 +54,7 @@ namespace KeepAliveMessaging
 				// Check if the response is a Keep Alive response
 				if (responseData.Length == 1 && responseData[0] == 0xFF)
 				{
-					Console.WriteLine("Received Keep Alive response from server");
+					Console.WriteLine("Received Keep-Alive response from server");
 				}
 				else
 				{
